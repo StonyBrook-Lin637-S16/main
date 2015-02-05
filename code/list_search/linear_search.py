b2b8a467c4fdfd4627f7106a519dd81249f45c65
Thomas Graf <git@thomasgraf.net>
2015-02-05 01:01:35 -0500

    rename searched_item to search_item

--- code/list_search/linear_search.py
+++ code/list_search/linear_search.py
@@ -2,10 +2,10 @@
 # encoding: utf-8
 
 
-def linear_search(search_list, searched_item):
+def linear_search(search_list, search_item):
     """Search trough list from left to right"""
     for i in range(len(search_list)):
-        if searched_item == search_list[i]:
+        if search_item == search_list[i]:
             print("Item found at index " + str(i))
             return i
     return False
